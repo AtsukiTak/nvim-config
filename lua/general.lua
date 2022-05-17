74a50bef1a43d04be52d4793762a7248544ef574
AtsukiTak <takatomgoo@gmail.com>
2022-05-17 16:56:50 +0900

    local optionを設定していた部分をglobal optionに変更

--- lua/general.lua
+++ lua/general.lua
@@ -3,11 +3,11 @@ vim.cmd("filetype plugin indent on")
 
 vim.o.number = true
 vim.o.ambiwidth = "double"
-vim.bo.tabstop = 2
-vim.bo.shiftwidth = 2
-vim.bo.softtabstop = 2
-vim.bo.autoindent = true
-vim.bo.expandtab = true -- tabを入力した時にspaceで代替する
-vim.wo.list = true -- tab文字などを可視化する
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.autoindent = true
+vim.o.expandtab = true -- tabを入力した時にspaceで代替する
+vim.o.list = true -- tab文字などを可視化する
 
 vim.cmd("colorscheme molokai")
