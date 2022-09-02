1001ae086891f4a25ad03b40e506a67aa7e79b5a
AtsukiTak <takatomgoo@gmail.com>
2022-09-02 18:14:04 +0900

    update

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -1,28 +1,6 @@
 vim.cmd[[packadd rust.vim]]
-vim.cmd[[packadd nvim-lspconfig]]
-
-lsp = require'lsp'
 
 local opts = { noremap=true, silent=true }
 
--- rust-analyzerの有効化
-require'lspconfig'.rust_analyzer.setup({
-  on_attach = lsp.on_attach_lsp,
-  flags = {
-    debounce_text_changes = 150,
-  },
-  settings = {
-    ["rust-analyzer"] = {
-      cargo = {
-        loadOutDirsFromCheck = true
-      }
-    }
-  }
-})
-
 -- keymap
-lsp.set_lsp_keymap()
 vim.api.nvim_set_keymap('n', 'qq', ':call rustfmt#Format()<CR>', opts)
-
--- command
-lsp.set_lsp_command()
--- init.lua
+++ init.lua
@@ -1,2 +1,57 @@
-require'general'
-require'keymap'
+vim.cmd("syntax enable")
+vim.cmd("filetype plugin indent on")
+
+vim.o.number = true
+vim.o.ambiwidth = "double"
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.autoindent = true
+vim.o.expandtab = true -- tabを入力した時にspaceで代替する
+vim.o.list = true -- tab文字などを可視化する
+
+vim.cmd("colorscheme molokai")
+
+-- ## Keymap
+local kmap_opts = { noremap=true, silent=true }
+
+-- カーソル移動系
+vim.api.nvim_set_keymap('', '<S-h>', '^', kmap_opts)
+vim.api.nvim_set_keymap('', '<C-a>', '^', kmap_opts)
+vim.api.nvim_set_keymap('', '<S-l>', '$', kmap_opts)
+vim.api.nvim_set_keymap('', '<C-e>', '$', kmap_opts)
+
+-- window管理系
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>w', kmap_opts)
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>W', kmap_opts)
+vim.api.nvim_set_keymap('n', '=', '<C-w>=', kmap_opts)
+
+-- buffer管理系
+vim.api.nvim_set_keymap('n', '<C-n>', ':bnext<CR>', kmap_opts)
+vim.api.nvim_set_keymap('n', '<C-p>', ':bprev<CR>', kmap_opts)
+vim.api.nvim_set_keymap('n', '<C-q>', ':bp|bd #<CR>', kmap_opts)
+
+-- lsp系
+vim.api.nvim_set_keymap('n', '<C-f>', '<cmd>lua vim.diagnostic.open_float()<CR>', kmap_opts)
+
+-- その他
+vim.api.nvim_set_keymap('i', '<C-j>', '<ESC>', kmap_opts)
+vim.api.nvim_set_keymap('', '<C-j>', '<ESC>', kmap_opts)
+
+
+-- ## "rust-tools.nvim" の設定
+require'rust-tools'.setup {
+  server = {
+    on_attach = function (client, bufnr)
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', kmap_opts)
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', kmap_opts)
+    end
+  }
+}
+
+
+-- ## "barbar.vim" の設定
+require'bufferline'.setup {
+  icons = 'numbers',
+  closable = false
+}
