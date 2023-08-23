288cc6a51c4a0ceb14234168f83317e0d1a2d41e
AtsukiTak <takatomgoo@gmail.com>
2023-08-23 14:13:02 +0900

    coc.nvimからnvim-lspに戻す

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -1,2 +1,29 @@
+vim.cmd[[packadd rust.vim]]
+vim.cmd[[packadd rust-tools.nvim]]
+
+local opts = { noremap=true, silent=true, buffer=true }
+
+-- keymap
+vim.keymap.set('n', 'qq', ':call rustfmt#Format()<CR>', opts)
+
 -- 保存時にrustfmtを実行する
-vim.g.rustfmt_autosave = 2
+vim.g.rustfmt_autosave = 1
+
+-- rust-tools.nvim
+require'rust-tools'.setup {
+  server = {
+    on_attach = function (client, bufnr)
+      vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+      vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    end,
+    settings = {
+      ["rust-analyzer"] = {
+        cargo = {
+          features = {},
+          target = nil
+        },
+        checkOnSave = true -- falseにしたときどうなるか確認する
+      }
+    }
+  }
+}
