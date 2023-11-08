b6320455c7d805933e6e54d7ad1903fce968ae1b
AtsukiTakahashi <takatomgoo@gmail.com>
2023-11-08 18:13:16 +0900

    コメント中のなどのキーワードを強調表示する

--- init.lua
+++ init.lua
@@ -15,7 +15,13 @@ vim.o.expandtab = true -- tabを入力した時にspaceで代替する
 vim.o.list = true -- tab文字などを可視化する
 vim.o.showcmd = false
 
+-- ## Color
 vim.cmd("colorscheme molokai")
+-- コメント中の `NOTE` などの文字を強調表示する
+-- See : https://zenn.dev/takatom/articles/cbb5a7f7996da0
+vim.api.nvim_set_hl(0, "@text.note", { link = "ModeMsg" })
+vim.api.nvim_set_hl(0, "@text.warning", { link = "Error" })
+vim.api.nvim_set_hl(0, "@text.danger", { link = "ErrorMsg" })
 
 -- ## Keymap
 local kmap_opts = { noremap=true, silent=true }
