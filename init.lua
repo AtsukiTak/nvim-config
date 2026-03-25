368a360d0cb7d71cf5332d5e1e2b67b03e1b9fc7
AtsukiTakahashi <takatomgoo@gmail.com>
2026-03-25 13:22:46 +0900

    set ambiwidth single

--- init.lua
+++ init.lua
@@ -6,7 +6,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.o.number = true
-vim.o.ambiwidth = "double"
+vim.o.ambiwidth = "single"
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.softtabstop = 2
