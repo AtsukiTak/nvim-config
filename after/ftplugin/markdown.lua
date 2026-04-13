ebf594db922e2b75b15b28804078553d46e41418
AtsukiTakahashi <takatomgoo@gmail.com>
2026-04-13 11:33:34 +0900

    fix markdown.lua

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,5 +1,5 @@
 -- Enter で list marker を継続させる
-vim.opt_local.formatoptions:append({ "r", "o" })
+vim.opt_local.formatoptions:append("ro")
 
 -- -, *, + を「次行でも継続する bullet」として扱う
 vim.opt_local.comments = "b:*,b:-,b:+,n:>"
